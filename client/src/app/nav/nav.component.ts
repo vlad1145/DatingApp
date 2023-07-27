760956adc6f45177d12a855c83d29c8e5da6562b
vlad1145 <pikulvu15@gmail.com>
2023-07-28 01:15:27 +0200

    Added the Like User Feature

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,9 @@ import { Component} from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { MembersService } from '../_services/members.service';
+import { UserParams } from '../_models/userParams';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-nav',
