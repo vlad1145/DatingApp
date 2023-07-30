dc7074370daa14d70bda25ca01fd61204ce74911
vlad1145 <pikulvu15@gmail.com>
2023-07-30 18:05:23 +0200

    Added Messaging Functionality

--- API/Controllers/MessagesController.cs
+++ API/Controllers/MessagesController.cs
@@ -69,5 +69,27 @@ namespace API.Controllers
 
             return Ok(await _messageRepository.GetMessageThread(currentUsername, username));
         }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMesssage(int id)
+        {
+            var username = User.GetUsername();
+
+            var message = await _messageRepository.GetMessage(id);
+
+            if(message.SenderUserName != username && message.RecipientUsername != username) return Unauthorized();
+
+            if(message.SenderUserName == username) message.SenderDeleted = true;
+            if(message.RecipientUsername == username) message.RecipientDeleted = true;
+
+            if(message.SenderDeleted && message.RecipientDeleted)
+            {
+                _messageRepository.DeleteMessage(message);
+            }
+
+            if(await _messageRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Problem deleting the message");
+        }
     }
 }
--- client/src/app/members/member-messages/member-messages.component.ts
+++ client/src/app/members/member-messages/member-messages.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Message } from 'src/app/_models/message';
 import { MessageService } from 'src/app/_services/message.service';
 
@@ -7,23 +8,30 @@ import { MessageService } from 'src/app/_services/message.service';
   templateUrl: './member-messages.component.html',
   styleUrls: ['./member-messages.component.css']
 })
-export class MemberMessagesComponent implements OnInit{
-  @Input() username?:  string;
-  messages: Message[] = [];
+export class MemberMessagesComponent implements OnInit {
+  @ViewChild('messageForm') messageForm?: NgForm
+  @Input() username?: string;
+  @Input() messages: Message[] = [];
+  messageContent = '';
 
 
-  constructor(private messageService: MessageService){}
+  constructor(private messageService: MessageService) { }
 
   ngOnInit(): void {
-    this.loadMessages();
+
   }
 
-  loadMessages(){
-    if(this.username){
-      this.messageService.getMessagesThread(this.username).subscribe({
-        next: messages => this.messages = messages
-      })
-    }
+  sendMessage() {
+    if (!this.username) return;
+
+    this.messageService.sendMessage(this.username, this.messageContent).subscribe({
+      next: message => {
+        this.messages.push(message);
+        this.messageForm?.reset();
+      }
+    })
   }
+
+
 
 }
