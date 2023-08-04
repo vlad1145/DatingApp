0550d70bbccf84c68c9eb3305a794f6069e022f0
vlad1145 <pikulvu15@gmail.com>
2023-08-04 13:25:04 +0200

    Added Unit Of Work Pattern and finished a touches

--- client/src/app/_guards/prevent-unsaved-changes.guard.ts
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,16 +1,34 @@
-import { Injectable } from '@angular/core';
-import { CanDeactivate } from '@angular/router';
+import { Injectable, inject } from '@angular/core';
+import { CanDeactivateFn } from '@angular/router';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { ConfirmService } from '../_services/confirm.service';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class PreventUnsavedChangesGuard implements CanDeactivate<MemberEditComponent> {
-  canDeactivate(component: MemberEditComponent): boolean {
-    if(component.editForm?.dirty) {
-      return confirm('Are you sure you want to continue? Any unsaved changes will be lost');
-    }
-    return true;
+export const preventUnsavedChangesGuard: CanDeactivateFn<MemberEditComponent> = (component) => {
+  const confirmService = inject(ConfirmService);
+
+  if(component.editForm?.dirty){
+    return confirmService.confirm();
   }
+
+  return true;
+};
+
+
+
+
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+
+// export class PreventUnsavedChangesGuard implements CanDeactivate<MemberEditComponent> {
+//   canDeactivate(component: MemberEditComponent): boolean {
+//     const confirmService = inject(ConfirmService);
+
+//     if(component.editForm?.dirty) {
+//       return confirmService.confirm();
+//     }
+//     return true;
+//   }
   
-}
+// }
